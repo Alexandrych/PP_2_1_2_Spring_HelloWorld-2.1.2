bea02a15b93bbfc73e662efe4acbc4fb181d97e1
Alexandrych <alexandrych@mail.ru>
2022-08-06 17:27:50 +0300

    v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean == bean1);
+
+        Cat barsik = (Cat) applicationContext.getBean("cat");
+        Cat murka = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(barsik == murka);
+
         System.out.println(bean.getMessage());
+        System.out.println(barsik.getMessage());
     }
 }
